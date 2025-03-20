8d4889a17760609405e0623fad4092be906e1c9a
Pierre SOUVIGNET <pierre.souvignet@inrae.fr>
2025-03-20 13:40:31 +0100

    rm markflat

--- src/DependencyInjection/MarkFlatEditorExtension.php
+++ src/DependencyInjection/MarkFlatEditorExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MarkFlat\MarkFlatEditor\DependencyInjection;
+namespace MarkFlatEditor\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
