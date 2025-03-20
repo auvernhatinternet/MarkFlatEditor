58ce4bb8b71b11cc79d0f448867962160aa06699
Pierre SOUVIGNET <pierre.souvignet@inrae.fr>
2025-03-20 13:36:29 +0100

    feat: second push + packagist

--- src/DependencyInjection/MarkFlatEditorExtension.php
+++ src/DependencyInjection/MarkFlatEditorExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MarkFlatEditor\DependencyInjection;
+namespace MarkFlat\MarkFlatEditor\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
