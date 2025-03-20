d5ee1762a0db9766e19245e3ec7f6deb36758cf9
Pierre SOUVIGNET <pierre.souvignet@inrae.fr>
2025-03-20 15:22:01 +0100

    fix extension

--- src/MarkFlatEditorBundle.php
+++ src/MarkFlatEditorBundle.php
@@ -2,10 +2,18 @@
 
 namespace MarkFlatEditor;
 
+use MarkFlatEditor\DependencyInjection\Compiler\TwigExtensionPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MarkFlatEditorBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new TwigExtensionPass());
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
